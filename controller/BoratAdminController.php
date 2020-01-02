69a652bcbd456667f22ae8c8d87d275f8c5d65c3
jonathan-martz <github@jmartz.de>
2020-01-03 00:36:23 +0100

    add logic for delete, reformat code

--- controller/BoratAdminController.php
+++ controller/BoratAdminController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Model\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
-use function base_path;
+use Illuminate\Validation\ValidationException;
 
 /**
  * Class BoratAdminController
@@ -13,7 +13,8 @@ use function base_path;
 class BoratAdminController extends Controller
 {
     /**
-     * @return mixed
+     * @return JsonResponse
+     * @throws ValidationException
      */
     public function edit()
     {
@@ -25,13 +26,35 @@ class BoratAdminController extends Controller
     }
 
     /**
-     * @return mixed
+     * @return JsonResponse
+     * @throws ValidationException
      */
     public function delete()
     {
-        $validation = $this->validate($this->request, []);
+        $validation = $this->validate($this->request, [
+            'id' => 'required|integer'
+        ]);
 
-        $this->addMessage('success', 'Package removed');
+        if($this->request->user()->getRoleName() === 'admin') {
+            $packages = DB::table('packages')->where('id', '=', $this->request->input('id'));
+
+            if($packages->count() === 1) {
+                $result = DB::table('packages')->delete($this->request->input('id'));
+
+                if($result) {
+                    $this->addMessage('success', 'Package removed');
+                }
+                else {
+                    $this->addMessage('error', 'Upps something went wrong. Try again later and Contact an Admin when the Error doesnt disappear later.');
+                }
+            }
+            else {
+                $this->addMessage('error', 'Package with id ' . $this->request->input('id') . ' doesnt exists.');
+            }
+        }
+        else {
+            $this->addMessage('error', 'Only admins can remove a package!');
+        }
 
         return $this->getResponse();
     }
